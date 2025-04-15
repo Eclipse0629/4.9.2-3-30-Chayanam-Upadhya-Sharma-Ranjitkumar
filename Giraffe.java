3d56baf50ccab04324deca38b3b1fe76fbb11140
varun-chayanam <varunchayanam@gmail.com>
2025-04-15 14:37:53 -0700

    Update Giraffe.java

--- Giraffe.java
+++ Giraffe.java
@@ -5,7 +5,14 @@ public class Giraffe extends Animal {
         super(food, nocturnal, aveLifeSpan);
         System.out.println("A giraffe arrives.");
     }
-
+    public void hum() {
+        System.out.println("The giraffe hums");
+    }
+    
+    @Override
+    public void speak() {
+        hum();
+    }
     // Override eat method to provide specific behavior
     @Override
     public void eat() {
