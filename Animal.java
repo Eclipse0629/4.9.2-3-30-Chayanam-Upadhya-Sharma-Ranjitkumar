d603f7dc3e4a5a8a243b279233f20e7c7679dfb5
varun-chayanam <varunchayanam@gmail.com>
2025-04-15 14:41:31 -0700

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -3,23 +3,23 @@ public class Animal {
     protected String food;
     protected boolean nocturnal;
     protected double aveLifeSpan;
-
+    
     // No-arg constructor
     public Animal() {
         System.out.println("An animal arrives.");
     }
-
-    public void speak() {
-        System.out.println("Animal makes a sound");
-    }
-
+    
     // Parameterized constructor
     public Animal(String food, boolean nocturnal, double aveLifeSpan) {
         this.food = food;
         this.nocturnal = nocturnal;
         this.aveLifeSpan = aveLifeSpan;
     }
-
+    
+    public void speak() {
+        System.out.println("Animal makes a sound");
+    }
+    
     public void eat() {
         System.out.println("The animal is eating.");
     }
